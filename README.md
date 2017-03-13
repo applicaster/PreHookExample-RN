6b135673f3e8ba0700e0191617bdd1d75362900b
Carlos Pinto <c.pinto@applicaster.com>
2017-03-13 16:25:20 -0400

    Initial commit with the entire project structure and basic dependencies.

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Native Feed
+
+This project is the Feed built using react native.
---
+++ src/App.ios.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          React Native Feed!
+        </Text>
+        <Text style={styles.subTitle}>
+          And this is nice
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#27DA86',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+  },
+  subTitle: {
+    textAlign: 'center',
+    color: '#ffffff',
+    marginBottom: 5,
+    opacity: 0.85,
+  },
+});
+
+export default App;
