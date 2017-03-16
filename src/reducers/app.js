7d04c9d338cdc90f4a62d3085fcb4e682da5efd5
Carlos Pinto <c.pinto@applicaster.com>
2017-03-16 09:49:35 -0400

    WIP reducer for app for setting accountId, environment, timelineId, and timezone.

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -2,11 +2,19 @@ import { Map } from 'immutable';
 import {
     START_LOADING_TIMELINES,
     DONE_LOADING_TIMELINES,
+    SET_ENVIRONMENT,
+    SET_ACCOUNT_ID,
+    SET_TIMELINE_ID,
+    SET_TIMEZONE,
 } from '../actions';
 
 
 const appInitialState = Map({
   loadingTimelines: false,
+  env: null,
+  accountId: null,
+  timelineId: null,
+  timezone: null,
 });
 
 export default (state = appInitialState, action) => {
@@ -19,6 +27,18 @@ export default (state = appInitialState, action) => {
     case DONE_LOADING_TIMELINES:
       return state.set('loadingTimelines', false);
     
+    case SET_ENVIRONMENT:
+      return state.set('env', payload.env);
+
+    case SET_ACCOUNT_ID:
+      return state.set('accountId', payload.accountId);
+    
+    case SET_TIMELINE_ID:
+      return state.set('timelineId', payload.timelineId);
+
+    case SET_TIMEZONE:
+      return state.set('timezone', payload.timezone);
+
     default:
       return state;
   }
