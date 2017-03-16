3f7328fea34f2f409fcb4459b5b4ec49de75b243
Carlos Pinto <c.pinto@applicaster.com>
2017-03-16 09:47:39 -0400

    Add selectors file with main initial selectors for app configuration values.

---
+++ src/selectors/index.js
@@ -0,0 +1,4 @@
+export const getAccountId = state => state.app.get('accountId');
+export const getTimelineId = state => state.app.get('timelineId');
+export const getTimezone = state => state.app.get('timezone');
+export const getStarsServiceState = state => state.app.get('starsServiceState');
