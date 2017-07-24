a92acee14e4b5195af0cd776257a40bb09757249
Carlos Pinto <c.pinto@applicaster.com>
2017-07-24 13:40:06 -0400

    Added spec file and test for social api call.

---
+++ test/api/social.spec.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { fetchSocialEvents } from '../../src/api/social';
+
+describe('api', () => {
+  describe('social', () => {
+    describe('fetchSocialEvents', () => {
+      beforeEach(() => {
+        sinon.stub(axios, 'get').resolves({ foo: 'bar' });
+
+        fetchSocialEvents({
+          environment: 'production',
+          accountId: 'someAccountId',
+          timelineId: 'someTimelineId',
+        });
+      });
+
+      it('should fetch with the correct url', () => {
+        expect(axios.get).to.have.been.calledOnce;
+        expect(axios.get).to.have.been.calledWith(
+          'http://assets-production.applicaster.com/social-media-aggregator//someAccountId/someTimelineId/instagram.json'
+        );
+      });
+
+      afterEach(() => {
+        axios.get.restore();
+      });
+    });
+  });
+});
