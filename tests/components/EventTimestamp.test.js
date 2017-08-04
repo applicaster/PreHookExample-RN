b2c9ea4e0b8e01c5cfd6a1be7c2a169ee521b416
Carlos Pinto <c.pinto@applicaster.com>
2017-08-04 16:26:24 -0400

    test(EventTimestamp): mocking time to a set point in time to make tests deterministic and fix ci.

--- tests/components/EventTimestamp.test.js
+++ tests/components/EventTimestamp.test.js
@@ -1,7 +1,10 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import sinon from 'sinon';
 import EventTimestamp from '../../src/components/EventTimestamp';
+
+sinon.useFakeTimers(new Date(1501878153000));
 
 describe('EventTimestamp', () => {
   test('component renders correctly for a timestamp within a week', () => {
