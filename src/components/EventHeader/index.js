db7f35d8c587098bfe241190a243459e78c49a45
Carlos Pinto <c.pinto@applicaster.com>
2017-08-07 17:33:24 -0400

    test(jest snapshots): resetting snapshots after changing the Icon component for Image.

--- src/components/EventHeader/index.js
+++ src/components/EventHeader/index.js
@@ -7,7 +7,6 @@ import {
   Text,
   View,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import EventTimestamp from '../EventTimestamp';
 
 const width = Dimensions.get('window').width;
