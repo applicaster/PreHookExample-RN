7b37716006b2bc572a79382d53dad7188156e7c8
Carlos Pinto <c.pinto@applicaster.com>
2017-08-07 17:28:28 -0400

    chore(App.ios): removing unused import.

--- src/App.ios.js
+++ src/App.ios.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Feed from './components/Feed';
