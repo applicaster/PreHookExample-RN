887d0ed8e529d64791d691fb0c306a49da3c7dfa
Carlos Pinto <c.pinto@applicaster.com>
2017-08-16 18:21:50 -0400

    feat(EventDetailCount): added EventDetailCount component for labels and count when no buttons are shown.

---
+++ src/components/EventDetailCount/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  Text,
+  View,
+ } from 'react-native';
+import hexToRgb from 'hex-to-rgb';
+
+const styles = StyleSheet.create({
+  eventDetailCount: {
+    flexDirection: 'row',
+    marginRight: 5,
+  },
+  count: {
+    fontSize: 12,
+    marginRight: 3,
+  },
+  label: {
+    fontSize: 12,
+  },
+});
+
+const EventDetailCount = ({ label, count = 0 }, { textColor }) => {
+  const rgb = hexToRgb(textColor || '#FFFFFF');
+  const rgbaColor = `rgba(${rgb[0]},${rgb[1]},${rgb[2]}, 0.75)`;
+  const textColorStyle = { color: rgbaColor };
+  return (
+    <View style={styles.eventDetailCount}>
+      <Text key={'count'} style={[styles.count, textColorStyle]}>{count}</Text>
+      <Text key={'label'} style={[styles.label, textColorStyle]}>{label}</Text>
+    </View>
+  );
+};
+
+EventDetailCount.propTypes = {
+  label: PropTypes.string,
+  count: PropTypes.number,
+};
+
+EventDetailCount.contextTypes = {
+  textColor: PropTypes.string,
+};
+
+export default EventDetailCount;
