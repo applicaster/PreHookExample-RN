88aa55beea5aa8de4f289458048fc47a6b563207
Carlos Pinto <c.pinto@applicaster.com>
2017-08-16 18:20:02 -0400

    feat(ActionBar): added ActionBar container for actions and details on event.

---
+++ tests/components/ActionBar.test.js
@@ -0,0 +1,20 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ActionBar from '../../src/components/ActionBar';
+
+describe('ActionBar', () => {
+  test('component renders correctly for instagram', () => {
+    const html = renderer.create(
+      <ActionBar socialNetwork={'instagram'} likesCount={1} commentsCount={3} />
+    ).toJSON();
+    expect(html).toMatchSnapshot();
+  });
+  
+  test('component renders correctly for no social network', () => {
+    const html = renderer.create(
+      <ActionBar socialNetwork={null} likesCount={null} commentsCount={null} />
+    ).toJSON();
+    expect(html).toMatchSnapshot();
+  });
+});
