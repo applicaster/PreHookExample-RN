745cbd9562f149df0a4899868779c8bd23c2f58c
Carlos Pinto <c.pinto@applicaster.com>
2017-09-06 19:04:22 -0400

    feat(CommentScreen): added work in progress comments screen rendering the event container and plain list of comments.

--- src/components/CommentsScreen/index.js
+++ src/components/CommentsScreen/index.js
@@ -1,53 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { toggleModal } from '../../actions';
+
 import {
-  Modal,
-  View,
-  ScrollView,
-  Text,
-} from 'react-native';
-import EventContainer from '../EventContainer';
-import MediaDetailsScreen from '../MediaDetailsScreen';
-// import { styles } from './style';
+  getActiveEvent,
+  getMediaModalVisibility,
+} from '../../selectors';
 
-class CommentsScreen extends Component {
-  static navigationOptions = ({ screenProps }) => ({
-    ...screenProps 
-  });
+import CommentsScreen from './CommentsScreen';
 
-  render() {
-    const { event } = this.props;
-    const { comments } = event.comments; 
-    const backgroundFeedColor = { backgroundColor: this.context.backgroundColor };
-    
-    return (
-        <ScrollView style={[backgroundFeedColor]}>
-          <EventContainer key={event.id} event={event} />
-          {comments.map(comment =>
-            <Text key={comment.id}>{comment.text}</Text>
-          )}
-          <Modal
-          animationType={'fade'}
-          transparent={false}
-          visible={isMediaModalVisible}
-          onRequestClose={toggleModal}
-        >
-          <MediaDetailsScreen />
-        </Modal>
-      </ScrollView>
-    );
-  }
-}
+const mapStateToProps = state => ({
+  event: getActiveEvent(state),
+  isMediaModalVisible: getMediaModalVisibility(state),
+});
 
-CommentsScreen.propTypes = {
-  event: PropTypes.object,
-  navigation: PropTypes.object,
-  isMediaModalVisible: PropTypes.bool,
-  toggleModal: PropTypes.func,
-};
+const mapDispatchToProps = dispatch => bindActionCreators({
+  toggleModal,
+}, dispatch);
 
-CommentsScreen.contextTypes = {
-  backgroundColor: PropTypes.string,
-};
-
-export default CommentsScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsScreen);
