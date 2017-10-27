bcd51902954cf8ed3b5ae740ff624ac12139807b
Carlos Pinto <c.pinto@applicaster.com>
2017-10-27 17:00:54 -0400

    style(EventContainer): removed line separator, added style file, and support for border radius.

---
+++ src/components/EventContainer/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  eventContainer: {
+    borderRadius: 5,
+    marginBottom: 5,
+    marginHorizontal: 5,
+    overflow: 'hidden',
+  },
+});
