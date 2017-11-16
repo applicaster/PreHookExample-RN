b2db3f3cf7203de60bf255fa90bf977aa372d582
Carlos Pinto <c.pinto@applicaster.com>
2017-11-16 12:07:44 -0500

    feat(MediaDetailsModal): pass the correct modal name to close.

--- src/components/MediaDetailsModal/MediaDetailsModal.js
+++ src/components/MediaDetailsModal/MediaDetailsModal.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Modal,
-} from 'react-native';
+import { Modal } from 'react-native';
 import MediaDetailsScreen from '../MediaDetailsScreen';
 
 class MediaDetailsModal extends Component {
