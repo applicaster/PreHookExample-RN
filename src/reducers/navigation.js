a21a089864a7275d2f46aaceeed54dcab400f5cc
Javier Tarazaga <javier-tarazaga@users.noreply.github.com>
2017-11-21 08:17:49 +0100

    Fixed double tap navigation issue (#43)
    * Fixed double tap navigation issue
    
    * Added initial state

--- src/reducers/navigation.js
+++ src/reducers/navigation.js
@@ -2,7 +2,21 @@ import Navigator from '../Navigator';
 
 const initialState = Navigator.router.getStateForAction(Navigator.router.getActionForPathAndParams('Feed'));
 
+const getCurrentRouteName = (state) => {
+  const route = state.routes[state.index];
+  return typeof route.index === 'undefined' ? route.routeName : getCurrentRouteName(route);
+};
+
 export default (state = initialState, action) => {
   const nextState = Navigator.router.getStateForAction(action, state);
+
+  // prevents navigating twice to the same route
+  if (state && nextState) {
+    const stateRouteName = getCurrentRouteName(state);
+    const nextStateRouteName = getCurrentRouteName(nextState);
+    return stateRouteName === nextStateRouteName ? state : nextState;
+  }
+
+  // Simply return the original `state` if `nextState` is null or undefined.
   return nextState || state;
 };
