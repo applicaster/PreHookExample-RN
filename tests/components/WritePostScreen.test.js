a75a4cc9d0cee0a8a082c4b09d35f06248061d3a
Carlos Pinto <c.pinto@applicaster.com>
2017-11-22 15:55:47 -0500

    feat(WritePostScreen): added ability tweet and post to Facebook whenever either of the features is available. Also update labels and styles accordingly.

--- tests/components/WritePostScreen.test.js
+++ tests/components/WritePostScreen.test.js
@@ -3,10 +3,26 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import WritePostScreen from '../../src/components/WritePostScreen/WritePostScreen';
 
+jest.mock('@applicaster/feed-rn-utils', () => 'FeedRNUtils');
+
 describe('WritePostScreen', () => {
   test('component renders correctly', () => {
     const tree = renderer.create(
       <WritePostScreen />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('component renders correctly when only twitter is available', () => {
+    const tree = renderer.create(
+      <WritePostScreen isTwitterAvailable isFacebookAvailable={false} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('component renders correctly when only facebook is available', () => {
+    const tree = renderer.create(
+      <WritePostScreen isTwitterAvailable={false} isFacebookAvailable />
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
