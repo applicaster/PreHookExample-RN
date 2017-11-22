02fb0cbebf1463ba03252249014c530da1ba9496
Carlos Pinto <c.pinto@applicaster.com>
2017-11-22 13:52:07 -0500

    feat(WritePostScreen): added post bar with close button, title and post button. Added overall styles for the entire screen.

--- src/components/WritePostScreen/WritePostScreen.js
+++ src/components/WritePostScreen/WritePostScreen.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import { styles } from './style';
 import CloseButton from '../CloseButton';
 
 class WritePostScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      text: '',
+      numberOfCharacters: 0,
+      postSelection: 'twitter',
+    };
     this.closeModal = this.closeModal.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(text) {
+    this.setState({ text });
+    this.setState({ numberOfCharacters: text.length });
   }
 
   closeModal() {
@@ -15,13 +32,67 @@ class WritePostScreen extends Component {
     toggleModal({ modal: 'WritePostModal' });
   }
 
+  renderActionBar() {
+    const { backgroundColor } = this.context;
+    const backgroundColorStyle = { backgroundColor };
+
+    return (
+      <View style={[styles.postOptionsBar, backgroundColorStyle]}></View>
+    );
+  }
+
+  renderPostBar() {
+    const { backgroundColor } = this.context;
+    const backgroundColorStyle = { backgroundColor };
+
+    return (
+      <View style={[styles.postBar, backgroundColorStyle]}>
+        <CloseButton style={styles.closeButton} onPress={this.closeModal} isForModal />
+        {this.renderWritePostLabel()}
+        {this.renderPostButton()}
+      </View>
+    );
+  }
+
+  renderWritePostLabel() {
+    const { mainColor } = this.context;
+    const textColorStyle = { color: mainColor };
+
+    return <Text style={[styles.writePostLabel, textColorStyle]}>Write a Post</Text>;
+  }
+
+  renderPostButton() {
+    const { textColor } = this.context;
+    const textColorStyle = { color: textColor };
+
+    return (
+      <TouchableOpacity style={styles.postButton} onPress={() => {} }>
+        <Text style={[styles.postButtonLabel, textColorStyle]}>Post</Text>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     const { backgroundColor } = this.context;
-    const screenBackgroundColor = { backgroundColor, flex: 1 };
+    const screenBackgroundColor = { backgroundColor };
     
     return (
       <View style={[styles.writePostScreen, screenBackgroundColor]}>
-        <CloseButton onPress={this.closeModal} isForModal />
+        
+        {this.renderPostBar()}
+        <KeyboardAvoidingView
+          behavior="padding"
+        >
+          <TextInput
+            autoFocus
+            multiline
+            style={styles.input}
+            placeholder="Write a post..."
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+          />
+          {this.renderActionBar()}
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -32,6 +103,7 @@ WritePostScreen.propTypes = {
 };
 
 WritePostScreen.contextTypes = {
+  mainColor: PropTypes.string,
   backgroundColor: PropTypes.string,
   textColor: PropTypes.string,
 };
--- src/components/WritePostScreen/style.js
+++ src/components/WritePostScreen/style.js
@@ -1,11 +1,47 @@
-import { StyleSheet } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+
+const window = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
   writePostScreen: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#323232',
+  },
+  input: {
+    height: 205,
+    fontSize: 20,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    paddingTop: 15,
+    width: window.width,
+  },
+  postBar: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: 65,
+    width: window.width,
+  },
+  postButton: {
+    position: 'absolute',
+    right: 12,
+    top: 32,
+  },
+  postButtonLabel: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  writePostLabel: {
+    fontWeight: '600',
+    fontSize: 18,
+    top: 32,
+  },
+  postOptionsBar: {
+    flexDirection: 'row',
+    height: 45,
+    width: window.width,
   },
 });
