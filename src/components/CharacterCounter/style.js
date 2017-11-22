f8f3634c13845134d7d52d27a109d9e4ce36420d
Carlos Pinto <c.pinto@applicaster.com>
2017-11-22 15:51:50 -0500

    feat(CharacterCounter): added counter component with logic and styles when characters are within limit or not.

---
+++ src/components/CharacterCounter/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  charactersRemainingContainer: {
+    borderRadius: 10,
+    flexDirection: 'column',
+    height: 22,
+    justifyContent: 'center',
+    marginLeft: 11,
+    paddingHorizontal: 5,
+  },
+  charactersRemainingLabel: {
+    textAlign: 'center',
+  },
+});
