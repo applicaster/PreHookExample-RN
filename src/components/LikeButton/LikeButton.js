a333d0f828107b85d827ad9c82a51cae049993a2
Carlos Pinto <c.pinto@applicaster.com>
2017-12-12 15:40:09 -0500

    feat(LikeButton): add LikeButton for the FacebookActionButtons bar.

---
+++ src/components/LikeButton/LikeButton.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ActionButton from '../ActionButton';
+import { LIKE_BUTTON } from '../../icons';
+
+class LikeButton extends Component {
+  constructor(props) {
+    super(props);
+    this.navigateToWebview = this.navigateToWebview.bind(this);
+  }
+
+  navigateToWebview() {
+    const { navigation } = this.context;
+    const { eventId, setActiveEventId } = this.props;
+
+    setActiveEventId(eventId);
+    navigation.navigate('WebView');
+  }
+  
+  render() {
+    const { likesCount } = this.props;
+    return (
+      <ActionButton
+        imageUri={LIKE_BUTTON}
+        label={likesCount}
+        onPress={this.navigateToWebview}
+      />
+    );
+  }
+}
+
+LikeButton.propTypes = {
+  eventId: PropTypes.string,
+  likesCount: PropTypes.number,
+  setActiveEventId: PropTypes.func,
+};
+
+LikeButton.contextTypes = {
+  navigation: PropTypes.object,
+};
+
+export default LikeButton;
--- src/icons.js
+++ src/icons.js
@@ -1,3 +1,5 @@
 export const RETWEET_ICON = 'feed_rn_retweet_button';
 export const COMMENT_BUTTON = 'feed_rn_comment_button';
+export const LIKE_BUTTON = 'feed_rn_like_button';
 export const FAVORITE_TWEET_BUTTON = 'feed_rn_heart_button';
+export const HEART_BUTTON = 'feed_rn_heart_button';
