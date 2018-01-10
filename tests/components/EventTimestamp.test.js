1c9e16a7fda906aa591f43d43249d9e798d0ea92
Carlos Pinto <c.pinto@applicaster.com>
2018-01-10 12:43:18 -0500

    tests(EventTimeStamp): fixing dates to correct tests.

--- tests/components/EventTimestamp.test.js
+++ tests/components/EventTimestamp.test.js
@@ -4,26 +4,26 @@ import renderer from 'react-test-renderer';
 import sinon from 'sinon';
 import EventTimestamp from '../../src/components/EventTimestamp';
 
-sinon.useFakeTimers(new Date(1501878153000));
+sinon.useFakeTimers(new Date(1533370943000));
 
 describe('EventTimestamp', () => {
   test('component renders correctly for a timestamp within a week', () => {
     const html = renderer.create(
-      <EventTimestamp timestamp={1501876350} />
+      <EventTimestamp timestamp={1501833150} />
     ).toJSON();
     expect(html).toMatchSnapshot();
   });
 
   test('component renders correctly for a timestamp within a year', () => {
     const html = renderer.create(
-      <EventTimestamp timestamp={1480881148} />
+      <EventTimestamp timestamp={1512373948} />
     ).toJSON();
     expect(html).toMatchSnapshot();
   });
 
   test('component renders correctly for a timestamp over a year', () => {
     const html = renderer.create(
-      <EventTimestamp timestamp={1449258748} />
+      <EventTimestamp timestamp={1480837948} />
     ).toJSON();
     expect(html).toMatchSnapshot();
   });
