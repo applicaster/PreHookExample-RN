251fa296bdc7c11eecea09f27847ee30bfa0614d
Carlos Pinto <c.pinto@applicaster.com>
2018-01-21 14:17:40 -0500

    feat(ShareButton): change to connected component and set localization values for default share messages and title.

---
+++ src/components/ShareButton/ShareButton.js
@@ -0,0 +1,70 @@
+import {
+  Image,
+  StyleSheet,
+  Share,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const buttonSize = 30;
+const styles = StyleSheet.create({
+  shareButton: {
+    width: buttonSize,
+    height: buttonSize,
+  },
+});
+
+class ShareButton extends Component {
+  constructor(props) {
+    super(props);
+    this.share = this.share.bind(this);
+  }
+
+  share() {
+    const {
+      defaultMessage = 'Download the app now!',
+      message = '',
+      url = 'http://its0n.tv/a/jb',
+      title = '',
+    } = this.props;
+
+    const textToShare = (Platform.OS === 'ios')
+    ? `${defaultMessage} - ${message}`
+    : `${defaultMessage} - ${message} ${url}`;
+
+    Share.share({
+      message: textToShare,
+      title,
+      url,
+    })
+    .then(() => {})
+    .catch(() => {});
+  }
+
+  render() {
+    const imageColor = { tintColor: `${this.context.textColor}99` };
+    return (
+      <TouchableOpacity onPress={this.share}>
+        <Image
+          style={[styles.shareButton, imageColor]}
+          source={{ uri: 'feed_rn_share_button' }}
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+ShareButton.propTypes = {
+  defaultMessage: PropTypes.string,
+  message: PropTypes.string,
+  title: PropTypes.string,
+  url: PropTypes.string,
+};
+
+ShareButton.contextTypes = {
+  textColor: PropTypes.string,
+};
+
+export default ShareButton;
--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,73 +1,10 @@
-import {
-  Image,
-  StyleSheet,
-  Share,
-  TouchableOpacity,
-  Platform,
-} from 'react-native';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import hexToRgb from 'hex-to-rgb';
+import { connect } from 'react-redux';
+import { getTranslations } from '../../selectors';
+import ShareButton from './ShareButton';
 
-const buttonSize = 30;
-const styles = StyleSheet.create({
-  shareButton: {
-    width: buttonSize,
-    height: buttonSize,
-  },
+const mapStateToProps = state => ({
+  defaultMessage: getTranslations(state).defaultShareMessage,
+  title: getTranslations(state).shareTitle,
 });
 
-class ShareButton extends Component {
-  constructor(props) {
-    super(props);
-    this.share = this.share.bind(this);
-  }
-
-  share() {
-    const {
-      defaultMessage = 'Download the app now!',
-      message = '',
-      url = 'http://its0n.tv/a/jb',
-      title = '',
-    } = this.props;
-
-    const textToShare = (Platform.OS === 'ios')
-    ? `${defaultMessage} - ${message}`
-    : `${defaultMessage} - ${message} ${url}`;
-
-    Share.share({
-      message: textToShare,
-      title,
-      url,
-    })
-    .then(() => {})
-    .catch(() => {});
-  }
-
-  render() {
-    const rgb = hexToRgb(this.context.textColor || '#FFFFFF');
-    const rgbaColor = `rgba(${rgb[0]},${rgb[1]},${rgb[2]}, 0.60)`;
-    const imageColor = { tintColor: rgbaColor };
-    return (
-      <TouchableOpacity onPress={this.share}>
-        <Image
-          style={[styles.shareButton, imageColor]}
-          source={{ uri: 'feed_rn_share_button' }}
-        />
-      </TouchableOpacity>
-    );
-  }
-}
-
-ShareButton.propTypes = {
-  defaultMessage: PropTypes.string,
-  message: PropTypes.string,
-  title: PropTypes.string,
-  url: PropTypes.string,
-};
-
-ShareButton.contextTypes = {
-  textColor: PropTypes.string,
-};
-
-export default ShareButton;
+export default connect(mapStateToProps, undefined)(ShareButton);
