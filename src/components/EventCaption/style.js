3a66bacd99ce83e57ad89115aea43a2d486ea141
Carlos Pinto <c.pinto@applicaster.com>
2018-01-22 13:34:43 -0500

    fix(EventCaption): changed margin to padding on text caption to fix on press event on links.

--- src/components/EventCaption/style.js
+++ src/components/EventCaption/style.js
@@ -3,9 +3,9 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   eventCaption: {
     fontSize: 14,
-    paddingHorizontal: 12,
-    paddingTop: 17,
-    paddingBottom: 5,
+    marginHorizontal: 12,
+    marginTop: 17,
+    marginBottom: 5,
     lineHeight: 18,
   },
 });
