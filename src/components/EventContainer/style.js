3a381c5ca20aa032530694efbbf9d53d4bda8143
Carlos Pinto <c.pinto@applicaster.com>
2018-04-11 15:48:10 -0400

    style(EventContainer): set color on shadow and lowered opacity to have a more subtle effect.

--- src/components/EventContainer/style.js
+++ src/components/EventContainer/style.js
@@ -10,7 +10,8 @@ export const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 25,
     marginHorizontal: SCREEN_MARGIN,
-    shadowOpacity: 0.2,
+    shadowOpacity: 0.13,
+    shadowColor: '#000000',
     shadowRadius: 8,
     shadowOffset: {
       width: 1,
