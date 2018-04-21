c0bda8b936aeb53ef058c77b1568625658f5b900
Carlos Pinto <c.pinto@applicaster.com>
2018-04-21 18:30:21 -0400

    fix(EventMedia): fixed width on gradient for media items.

--- src/components/EventMedia/style.js
+++ src/components/EventMedia/style.js
@@ -2,12 +2,14 @@ import {
   Dimensions,
   StyleSheet,
 } from 'react-native';
+import { SCREEN_MARGIN } from '../../constants/measurements';
 
 const screenWidth = Dimensions.get('window').width;
+
 export const styles = StyleSheet.create({
   headerVisor: {
     height: 130,
-    width: screenWidth,
+    width: screenWidth - (2 * SCREEN_MARGIN),
     position: 'absolute',
     top: 0,
     left: 0,
