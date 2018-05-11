faafee3426abf6df35e09e4f8c4873427be1e9a1
Carlos Pinto <c.pinto@applicaster.com>
2018-05-11 19:37:13 -0400

    fix(MediaVideo): adding back missing styles found due to refactoring.

--- src/screens/HomeScreen/cards/VideoCard/MediaVideo/style.js
+++ src/screens/HomeScreen/cards/VideoCard/MediaVideo/style.js
@@ -1,4 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+import { SCREEN_MARGIN } from '../../../../../constants/measurements';
+
+const screenWidth = Dimensions.get('window').width;
 
 export const styles = StyleSheet.create({
   eventVideo: {
@@ -6,5 +9,13 @@ export const styles = StyleSheet.create({
     top: 0,
     left: 0,
     zIndex: 1,
+  },
+  headerVisor: {
+    height: 130,
+    width: screenWidth - (2 * SCREEN_MARGIN),
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 2,
   },
 });
