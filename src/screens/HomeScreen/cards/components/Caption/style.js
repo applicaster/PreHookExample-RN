ebd8180adb3d9041446f88dfe10feec648bd51d1
Carlos Pinto <c.pinto@applicaster.com>
2018-05-16 12:20:26 -0400

    style(Caption): set font size to match new design.

--- src/screens/HomeScreen/cards/components/Caption/style.js
+++ src/screens/HomeScreen/cards/components/Caption/style.js
@@ -2,10 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   eventCaption: {
-    fontSize: 14,
+    fontSize: 18,
     marginHorizontal: 12,
     marginTop: 17,
     marginBottom: 5,
-    lineHeight: 18,
   },
 });
