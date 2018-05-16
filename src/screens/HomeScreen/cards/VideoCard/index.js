552593da212e7313b58e396f7e994bc2d643ceb7
Carlos Pinto <c.pinto@applicaster.com>
2018-05-16 17:44:02 -0400

    refactor(MediaVideo): move MediaVideo to components to be reused by article card.

--- src/screens/HomeScreen/cards/VideoCard/index.js
+++ src/screens/HomeScreen/cards/VideoCard/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-import MediaVideo from './MediaVideo';
+import MediaVideo from '../components/MediaVideo';
 import Caption from '../components/Caption';
 import CardContainer from '../components/CardContainer';
 import Footer from '../components/Footer';
