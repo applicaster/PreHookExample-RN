f1abbe1010e4af40960fb09575e78cb376eb76a9
Carlos Pinto <c.pinto@applicaster.com>
2018-05-16 17:24:48 -0400

    style(Caption): adjust line height value.

--- src/screens/HomeScreen/cards/components/Caption/style.js
+++ src/screens/HomeScreen/cards/components/Caption/style.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   eventCaption: {
     fontSize: 18,
+    lineHeight: 22,
     marginHorizontal: 12,
     marginTop: 17,
     marginBottom: 5,
