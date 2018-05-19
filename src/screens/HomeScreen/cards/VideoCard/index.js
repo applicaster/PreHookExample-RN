13841b1e8d20b2bce9582c29e3e57bf4d2aaf197
Carlos Pinto <c.pinto@applicaster.com>
2018-05-19 00:30:49 -0400

    feat(VideoCard): add conditional title for editorial video cards.

--- src/screens/HomeScreen/cards/VideoCard/index.js
+++ src/screens/HomeScreen/cards/VideoCard/index.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import MediaVideo from '../components/MediaVideo';
 import Caption from '../components/Caption';
 import CardContainer from '../components/CardContainer';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { styles as videoStyles } from './style';
 import { styles } from '../style';
 
 export default class VideoCard extends Component {
+  getTitleColor() {
+    const COLOR_CHANGE_TRESHHOLD = 0x999999;
+    const { backgroundColor } = this.context;
+    const backgroundColorValue = parseInt(backgroundColor.substring(1), 16);
+    if (backgroundColorValue < COLOR_CHANGE_TRESHHOLD) {
+      return '#FFFFFF';
+    }
+
+    return '#000000';
+  }
+
   activateCard() {
-    // should open image screen
-    // passed to CardContainer
+    // should open video full screen
+  }
+
+  renderVideoTitle() {
+    const { caption, isEditorial } = this.props;
+    if (isEditorial) {
+      return <Text style={[videoStyles.videoTitle, { color: this.getTitleColor() }]}>{caption}</Text>;
+    }
+
+    return <Caption caption={caption} />;
   }
 
   render() {
-    const { caption, eventId, imageHeight, imageUrl, imageWidth, videoUrl } = this.props;
+    const { eventId, imageHeight, imageUrl, imageWidth, isEditorial, videoUrl } = this.props;
     const backgroundColorStyle = { backgroundColor: this.context.backgroundColor };
-
+    
     return (
-      <CardContainer>
+      <CardContainer clickable clickHandler={() => this.activateCard()}>
         <View style={[styles.eventContainer, backgroundColorStyle]}>
           <Header eventId={eventId} overlay />
           <MediaVideo
@@ -29,8 +49,8 @@ export default class VideoCard extends Component {
             videoUrl={videoUrl}
             width={imageWidth}
           />
-          <Caption caption={caption} />
-          <Footer eventId={eventId} />
+          {this.renderVideoTitle()}
+          {!isEditorial && <Footer eventId={eventId} />}
         </View>
       </CardContainer>);
   }
@@ -42,6 +62,7 @@ VideoCard.propTypes = {
   imageHeight: PropTypes.number.isRequired,
   imageUrl: PropTypes.string.isRequired,
   imageWidth: PropTypes.number.isRequired,
+  isEditorial: PropTypes.bool.isRequired,
   videoUrl: PropTypes.string.isRequired,
 };
 
---
+++ src/screens/HomeScreen/cards/VideoCard/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  videoTitle: {
+    fontSize: 23,
+    fontWeight: '800',
+    paddingHorizontal: 13,
+    marginTop: 8,
+    marginBottom: 20,
+    color: '#000000',
+  },
+});
