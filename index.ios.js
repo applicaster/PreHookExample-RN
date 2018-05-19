89c91d1e281e7d1a35da5a70c3de46f39c188593
Carlos Pinto <c.pinto@applicaster.com>
2018-05-19 19:11:05 -0400

    feat(index.ios.js): set SafeAreaView for iOS devices.

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform, SafeAreaView } from 'react-native';
+import PropTypes from 'prop-types';
 
 import App from './src/App';
 
-const RNRoot = props => (
-  <App { ...props } platform="iOS" />
-);
+const RNRoot = props => {
+  let { extra_props: initialAppProps } = props;
+  if (Platform.OS === 'android') {
+    initialAppProps = JSON.parse(initialAppProps);
+  }
+
+  const { colors } = initialAppProps;
+  const backgroundColor = `#${colors.background_color.substring(2, 8)}`;
+
+  return (
+    <SafeAreaView style={{ backgroundColor, flex: 1 }}>
+      <App { ...props } platform="iOS" />
+    </SafeAreaView>
+  );
+};
+
+RNRoot.propTypes = {
+  extra_props: PropTypes.object,
+};
 
 AppRegistry.registerComponent('RNRoot', () => RNRoot);
