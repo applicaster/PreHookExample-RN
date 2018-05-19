db988e3ca30fb72475c4fc81c5fc4127f951101e
Carlos Pinto <c.pinto@applicaster.com>
2018-05-19 19:10:38 -0400

    chore(Navigator): remove header.

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,4 +1,4 @@
 import { StackNavigator } from 'react-navigation';
 import navigationRoutes from './navigationRoutes';
 
-export default StackNavigator(navigationRoutes);
+export default StackNavigator(navigationRoutes, { headerMode: 'none' });
