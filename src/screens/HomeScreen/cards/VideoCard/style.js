8e6bb3bf4bde695859df6d567cc540562bd0ba64
Carlos Pinto <c.pinto@applicaster.com>
2018-05-21 14:43:50 -0400

    style(VideoCard): font and spacing changes.

--- src/screens/HomeScreen/cards/VideoCard/style.js
+++ src/screens/HomeScreen/cards/VideoCard/style.js
@@ -2,11 +2,20 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   videoTitle: {
-    fontSize: 23,
+    fontSize: 21,
+    lineHeight: 24,
     fontWeight: '800',
     paddingHorizontal: 13,
-    marginTop: 12,
-    marginBottom: 20,
+    marginTop: 6,
+    marginBottom: 0,
     color: '#000000',
+  },
+  category: {
+    fontSize: 14,
+    color: '#4A4A4A',
+    opacity: 0.75,
+    fontWeight: 'bold',
+    paddingHorizontal: 13,
+    marginTop: 16,
   },
 });
