b28d6c9a713e6f1c2c06be5ca6df3933deede00b
Carlos Pinto <c.pinto@applicaster.com>
2018-05-23 15:57:39 -0400

    feat(MediaImage): added animated components when image is expanded and unexpanded.

--- src/screens/HomeScreen/cards/components/MediaImage/index.js
+++ src/screens/HomeScreen/cards/components/MediaImage/index.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Image, View } from 'react-native';
+import { Animated, View } from 'react-native';
 import LinearGradient from '@applicaster/react-native-linear-gradient';
 import { styles } from './style';
 import { getMediaDimensions } from '../../../../../utils/size';
 import { SCREEN_MARGIN } from '../../../../../constants/measurements';
 
 class MediaImage extends Component {
+  constructor(props) {
+    super(props);
+    this.imageAnimatedValue = new Animated.Value(1);
+    this.grandientAnimatedValue = new Animated.Value(1);
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    const { isExpanded: nextIsExpanded } = nextProps;
+    const { isExpanded } = this.props;
+  
+    if (nextIsExpanded !== isExpanded) {
+      Animated.parallel([
+        Animated.timing(
+          this.imageAnimatedValue,
+          {
+            toValue: (nextIsExpanded) ? 0 : 1,
+            duration: 500,
+          }),
+        Animated.timing(
+          this.grandientAnimatedValue,
+          {
+            toValue: (nextIsExpanded) ? 0 : 1,
+            duration: (nextIsExpanded) ? 200 : 1000,
+          }),
+      ]).start();
+    }
+  }
+
+  getImageStyles() {
+    const { isZoomed, height, width } = this.props;
+    const imageDimensions = getMediaDimensions({ height, width, screenMargin: SCREEN_MARGIN, isZoomed });
+    const leftValueWhenZoomed = this.imageAnimatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, -SCREEN_MARGIN],
+    });
+
+    return {
+      height: imageDimensions.height,
+      width: imageDimensions.width,
+      left: (isZoomed) ? leftValueWhenZoomed : 0,
+    };
+  }
+
   render() {
-    const { imageUrl, isZoomed, height, width } = this.props;
+    const { imageUrl } = this.props;
+    const imageStyles = this.getImageStyles();
+    const headerVisorContainerStyles = {
+      opacity: this.grandientAnimatedValue,
+      position: 'absolute',
+    };
 
     return (
       <View>
-        <Image
-          style={getMediaDimensions({ height, width, screenMargin: SCREEN_MARGIN, isZoomed })}
+        <Animated.Image
+          style={imageStyles}
           source={{ uri: imageUrl }}
         />
-        <LinearGradient style={ styles.headerVisor } colors={['rgba(0,0,0,0.7)', 'transparent']} />
+        <Animated.View style={headerVisorContainerStyles}>
+          <LinearGradient style={ styles.headerVisor } colors={['rgba(0,0,0,0.7)', 'transparent']} />
+        </Animated.View>
       </View>);
   }
 }
@@ -24,12 +74,14 @@ class MediaImage extends Component {
 MediaImage.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   isZoomed: PropTypes.bool.isRequired,
+  isExpanded: PropTypes.bool.isRequired,
   height: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
 };
 
 MediaImage.defaultProps = {
   isZoomed: false,
+  isExpanded: false,
 };
 
 export default MediaImage;
