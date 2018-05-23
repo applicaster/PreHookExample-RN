427c86a6150fd8b998572907ef3e11ef4b0773fe
Carlos Pinto <c.pinto@applicaster.com>
2018-05-23 13:44:40 -0400

    style(ArticleCard): use constant for horizontal padding on text items and added body article definition.

--- src/screens/HomeScreen/cards/ArticleCard/style.js
+++ src/screens/HomeScreen/cards/ArticleCard/style.js
@@ -1,19 +1,20 @@
 import { StyleSheet } from 'react-native';
 
+const HORIZONTAL_PADDING = 13;
 export const styles = StyleSheet.create({
   category: {
     fontSize: 14,
     color: '#4A4A4A',
     opacity: 0.75,
     fontWeight: 'bold',
-    paddingHorizontal: 13,
+    paddingHorizontal: HORIZONTAL_PADDING,
     marginTop: 16,
   },
   title: {
     fontSize: 21,
     lineHeight: 24,
     fontWeight: '800',
-    paddingHorizontal: 13,
+    paddingHorizontal: HORIZONTAL_PADDING,
     marginTop: 6,
     color: '#000000',
   },
@@ -21,8 +22,11 @@ export const styles = StyleSheet.create({
     fontSize: 18,
     lineHeight: 23,
     color: '#353333',
-    paddingHorizontal: 13,
+    paddingHorizontal: HORIZONTAL_PADDING,
     marginTop: 6,
     marginBottom: 0,
+  },
+  body: {
+    marginHorizontal: HORIZONTAL_PADDING,
   },
 });
