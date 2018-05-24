560e07fc3943746bd46adbd4b4c2c3d66448bdcb
Carlos Pinto <c.pinto@applicaster.com>
2018-05-24 18:21:16 -0400

    chore(icons): added CLOSE round button.

--- src/icons.js
+++ src/icons.js
@@ -6,3 +6,4 @@ export const HEART_BUTTON = 'feed_rn_heart_button';
 export const VIDEO_AUDIO_MUTED_BUTTON = 'feed_rn_video_audio_muted_button';
 export const VIDEO_AUDIO_ON_BUTTON = 'feed_rn_video_audio_on_button';
 export const PLAY_VIDEO_OVERLAY = 'feed_rn_play_video_overlay';
+export const CLOSE_ROUND_BUTTON = 'feed_rn_close_round_button';
