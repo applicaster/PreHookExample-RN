e49c9c534c175b7706a1b0c744df4bbc7e4b7d3a
Carlos Pinto <c.pinto@applicaster.com>
2018-05-25 00:43:57 -0400

    refactor(measurements): factor out card list top padding to constant.

--- src/constants/measurements.js
+++ src/constants/measurements.js
@@ -1,2 +1,3 @@
 export const SCREEN_MARGIN = 20;
 export const BORDER_RADIUS = 15;
+export const TOP_CARD_LIST_PADDING = 15;
--- src/screens/HomeScreen/style.js
+++ src/screens/HomeScreen/style.js
@@ -1,11 +1,10 @@
-import {
-  StyleSheet,
-} from 'react-native';
+import { StyleSheet } from 'react-native';
+import { TOP_CARD_LIST_PADDING } from '../../constants/measurements';
 
 export const styles = StyleSheet.create({
   feedListContent: {
     backgroundColor: 'transparent',
-    paddingTop: 15,
+    paddingTop: TOP_CARD_LIST_PADDING,
   },
   feedList: {
     backgroundColor: 'transparent',
