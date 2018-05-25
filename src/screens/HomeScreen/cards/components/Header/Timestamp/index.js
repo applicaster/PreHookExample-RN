0e6d558012b2f241c01a1130b682fc3a1e231507
Carlos Pinto <c.pinto@applicaster.com>
2018-05-24 22:41:14 -0400

    refactor(time, Timestamp): refactored time utils to support timeFromNow and remove from Timestamp component.

--- src/screens/HomeScreen/cards/components/Header/Timestamp/index.js
+++ src/screens/HomeScreen/cards/components/Header/Timestamp/index.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native';
-import moment from 'moment-with-locales-es6';
-import {
-  isWithinAWeek,
-  isWithinAYear,
-} from '../../../../../../utils/time';
+import { timeFromNow } from '../../../../../../utils/time';
 import { styles } from './style';
 
 class Timestamp extends Component {
-  renderTime() {
-    moment.locale('es');
-    const timestamp = this.props.timestamp;
-    const eventMoment = moment.unix(timestamp);
-
-    if (isWithinAWeek(eventMoment)) {
-      return eventMoment.fromNow();
-    } else if (isWithinAYear(eventMoment)) {
-      return eventMoment.format('MMMM D');
-    }
-    
-    return eventMoment.format('MMMM D, YYYY');
-  }
-  
   render() {
-    const { colorStyle } = this.props;
-    const time = this.renderTime();
+    const { colorStyle, timestamp } = this.props;
+    const time = timeFromNow(timestamp);
     const capitalizeFirstLetter = (string) => string[0].toUpperCase() + string.slice(1);
     
     return (<Text style={[styles.timestamp, colorStyle]}>{capitalizeFirstLetter(time)}</Text>);
--- src/utils/time.js
+++ src/utils/time.js
@@ -9,3 +9,15 @@ const A_YEAR_OLD = REFERENCE.clone().subtract(1, 'year').startOf('year');
 export const isWithinAWeek = (momentDate) => momentDate.isAfter(A_WEEK_OLD);
 export const isWithinAMonth = (momentDate) => momentDate.isAfter(A_MONTH_OLD);
 export const isWithinAYear = (momentDate) => momentDate.isAfter(A_YEAR_OLD);
+
+export const timeFromNow = (timestamp) => {
+  const eventMoment = moment.unix(timestamp);
+
+  if (isWithinAWeek(eventMoment)) {
+    return eventMoment.fromNow();
+  } else if (isWithinAYear(eventMoment)) {
+    return eventMoment.format('MMMM D');
+  }
+  
+  return eventMoment.format('MMMM D, YYYY');
+};
