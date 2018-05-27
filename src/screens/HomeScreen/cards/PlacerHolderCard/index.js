d48576ddc13df106190ce44e30bcafdc556a9b4f
Carlos Pinto <c.pinto@applicaster.com>
2018-05-27 02:47:05 -0400

    feat(PlaceHolderCard): added placeholder card component when cards are loading.

---
+++ src/screens/HomeScreen/cards/PlacerHolderCard/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Dimensions, View } from 'react-native';
+import CardContainer from '../components/CardContainer';
+import { styles } from '../style';
+import { styles as placeholderStyles } from './style';
+import { SCREEN_MARGIN } from '../../../../constants/measurements';
+
+const width = Dimensions.get('window').width;
+const cardWidth = width - (2 * SCREEN_MARGIN);
+
+export default class PlaceHolderCard extends Component {
+  renderLine({ height, spacing, widthPercentage }) {
+    const lineDimensionStyles = {
+      backgroundColor: this.context.secondaryTextColor,
+      height,
+      width: ((cardWidth - 30) * (widthPercentage / 100)),
+      marginBottom: spacing,
+    };
+
+    return <View style={[placeholderStyles.ghostLine, lineDimensionStyles]} />;
+  }
+
+  render() {
+    const backgroundColorStyle = { backgroundColor: this.context.backgroundColor };
+    const ghostBackgroundStyle = { backgroundColor: this.context.secondaryTextColor };
+
+    return (
+      <CardContainer clickable clickHandler={() => {}}>
+        <View style={[styles.eventContainer, placeholderStyles.ghostCard, backgroundColorStyle]}>
+          <View style={[placeholderStyles.ghostMedia, ghostBackgroundStyle]} />
+          {this.renderLine({ height: 12, spacing: 7, widthPercentage: 40 })}
+          {this.renderLine({ height: 18, spacing: 5, widthPercentage: 96 })}
+          {this.renderLine({ height: 18, spacing: 12, widthPercentage: 65 })}
+          {this.renderLine({ height: 10, spacing: 5, widthPercentage: 100 })}
+          {this.renderLine({ height: 10, spacing: 5, widthPercentage: 97 })}
+          {this.renderLine({ height: 10, spacing: 15, widthPercentage: 100 })}
+        </View>
+      </CardContainer>);
+  }
+}
+
+PlaceHolderCard.contextTypes = {
+  backgroundColor: PropTypes.string,
+  secondaryTextColor: PropTypes.string,
+};
---
+++ src/screens/HomeScreen/cards/PlacerHolderCard/style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { SCREEN_MARGIN } from '../../../../constants/measurements';
+import { getMediaDimensions } from '../../../../utils/size';
+
+const BORDER_RADIUS_FOR_GHOSTS = 5;
+const ghostMediaDimensions = getMediaDimensions({ height: 9, width: 16, screeMargin: SCREEN_MARGIN });
+export const styles = StyleSheet.create({
+  ghostCard: {
+    opacity: 0.3,
+    paddingHorizontal: 15,
+  },
+  ghostMedia: {
+    borderRadius: BORDER_RADIUS_FOR_GHOSTS,
+    height: ghostMediaDimensions.height,
+    marginBottom: 8,
+    marginTop: 15,
+  },
+  ghostLine: {
+    borderRadius: BORDER_RADIUS_FOR_GHOSTS,
+  },
+});
