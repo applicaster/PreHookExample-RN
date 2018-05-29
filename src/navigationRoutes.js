8dca096088ba4e864e7043ba80e7dc3da9a1d89d
Carlos Pinto <c.pinto@applicaster.com>
2018-05-29 00:16:39 -0400

    feat(navigationRoutes): add route for ArticleScreen.

--- src/navigationRoutes.js
+++ src/navigationRoutes.js
@@ -1,15 +1,19 @@
+import ArticleScreen from './screens/ArticleScreen';
+import GenericWebViewScreen from './screens/GenericWebViewScreen';
 import HomeScreen from './screens/HomeScreen';
 import SocialWebViewScreen from './screens/SocialWebViewScreen';
-import GenericWebViewScreen from './screens/GenericWebViewScreen';
 
 export default {
+  Article: {
+    screen: ArticleScreen,
+  },
+  GenericWebView: {
+    screen: GenericWebViewScreen,
+  },
   Home: {
     screen: HomeScreen,
   },
   SocialWebView: {
     screen: SocialWebViewScreen,
-  },
-  GenericWebView: {
-    screen: GenericWebViewScreen,
   },
 };
