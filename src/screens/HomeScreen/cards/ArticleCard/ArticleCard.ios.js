4425ff06e58df623ec7ad1cab108c63447e7250f
Carlos Pinto <c.pinto@applicaster.com>
2018-05-31 02:06:30 -0400

    style(ArticleCard): adjust time duration on opacity for summary.

--- src/screens/HomeScreen/cards/ArticleCard/ArticleCard.ios.js
+++ src/screens/HomeScreen/cards/ArticleCard/ArticleCard.ios.js
@@ -89,7 +89,7 @@ export default class ArticleCard extends Component {
           }),
           Animated.timing(this.opacityAnimationValue, {
             toValue: isCardActive ? 1 : 0,
-            duration: duration * (isCardActive ? 2 : 0.1),
+            duration,
             useNativeDriver: true,
           }),
           Animated.spring(this.transformCardAnimationValue, {
