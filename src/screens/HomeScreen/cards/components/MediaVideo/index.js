60fcaba80d061b61856bdd981cf3fe5526850715
Carlos Pinto <c.pinto@applicaster.com>
2018-06-09 00:45:30 -0400

    feat(MediaVideo): remove passing isZoomed on connector.

--- src/screens/HomeScreen/cards/components/MediaVideo/index.js
+++ src/screens/HomeScreen/cards/components/MediaVideo/index.js
@@ -1,17 +1,15 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { setEventIdForActiveAudio } from '../../../../../actions';
-import { getCards, getEventIdForActiveAudio, getViewableItems } from '../../../../../selectors';
+import { getEventIdForActiveAudio, getViewableItems } from '../../../../../selectors';
 import MediaVideo from './MediaVideo';
 
 const mapStateToProps = (state, props) => {
   const { eventId } = props;
-  const event = getCards(state)[eventId];
 
   return {
     eventIdForActiveAudio: getEventIdForActiveAudio(state),
     isInViewport: !!getViewableItems(state)[eventId],
-    isZoomed: (event.source === 'zappPipes'),
   };
 };
 
