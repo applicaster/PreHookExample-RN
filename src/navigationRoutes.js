235affed697f0bc2798abac499f7b0335189435d
Carlos Pinto <c.pinto@applicaster.com>
2018-06-19 17:55:19 -0400

    chore(navigationRoutes): added navigation route for VideoScreen and adjusted ArticleSCreen navigation route.

--- src/navigationRoutes.js
+++ src/navigationRoutes.js
@@ -2,10 +2,14 @@ import ArticleScreen from './screens/ArticleScreen';
 import GenericWebViewScreen from './screens/GenericWebViewScreen';
 import HomeScreen from './screens/HomeScreen';
 import SocialWebViewScreen from './screens/SocialWebViewScreen';
+import VideoScreen from './screens/VideoScreen';
 
 export default {
-  Article: {
+  'ArticleScreen': {
     screen: ArticleScreen,
+  },
+  'VideoScreen': {
+    screen: VideoScreen,
   },
   GenericWebView: {
     screen: GenericWebViewScreen,
