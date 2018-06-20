bfb10f63082cad73b47503a8de412aa71d326b7e
Carlos Pinto <c.pinto@applicaster.com>
2018-06-20 14:58:01 -0400

    refactor(navigationRoutes): remove video screen route.

--- src/navigationRoutes.js
+++ src/navigationRoutes.js
@@ -2,14 +2,10 @@ import ArticleScreen from './screens/ArticleScreen';
 import GenericWebViewScreen from './screens/GenericWebViewScreen';
 import HomeScreen from './screens/HomeScreen';
 import SocialWebViewScreen from './screens/SocialWebViewScreen';
-import VideoScreen from './screens/VideoScreen';
 
 export default {
   'ArticleScreen': {
     screen: ArticleScreen,
-  },
-  'VideoScreen': {
-    screen: VideoScreen,
   },
   GenericWebView: {
     screen: GenericWebViewScreen,
