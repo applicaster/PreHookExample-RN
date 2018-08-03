87c358e4edad16d2840509c32ecdabf75603e402
Carlos Pinto <c.pinto@applicaster.com>
2018-08-03 13:03:51 -0400

    feat(LinkCard): display link icon and take into account if it's social or not to adjust its top position.

--- src/screens/HomeScreen/cards/LinkCard/index.js
+++ src/screens/HomeScreen/cards/LinkCard/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import MediaImage from '../components/MediaImage';
 import CardContainer from '../components/CardContainer';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Caption from '../components/Caption';
 import { styles } from '../style';
+import { styles as linkStyles } from './style';
 
 export default class LinkCard extends Component {
   activateCard() {
@@ -25,10 +26,15 @@ export default class LinkCard extends Component {
         width={imageWidth}
       />)
       : null;
+    
+    const overlayContentOverHeader = !!imageUrl;
+    const isSocial = (source === 'twitter' || source === 'facebook' || source === 'instagram');
+    const linkTintColorStyle = { tintColor: (overlayContentOverHeader) ? this.context.secondaryColor : this.context.textColor };
 
     return (
       <CardContainer clickable clickHandler={() => this.activateCard()}>
         <View style={[styles.eventContainer, backgroundColorStyle]}>
+          <Image style={[linkStyles.linkIcon, linkTintColorStyle, isSocial && linkStyles.socialLink]} source={require('../../../../assets/images/link_icon.png')} />
           <Header eventId={eventId} overlay={!!imageUrl} />
           {mediaImage}
           {source !== 'cms' && <Caption caption={caption} />}
@@ -57,5 +63,7 @@ LinkCard.defaultProps = {
 
 LinkCard.contextTypes = {
   backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  secondaryColor: PropTypes.string,
   navigation: PropTypes.object,
 };
---
+++ src/screens/HomeScreen/cards/LinkCard/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  linkIcon: {
+    position: 'absolute',
+    right: 11,
+    top: 5,
+    height: 26,
+    width: 26,
+    zIndex: 1,
+  },
+  socialLink: {
+    top: 37,
+  },
+});
