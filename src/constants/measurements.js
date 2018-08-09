8e636eaf001f20060147f70945c45c56cd071705
Manuel Pretelin <m_pretelin@hotmail.com>
2018-08-09 12:34:13 -0500

    feat(measurements): add condition for square border in BORDER_RADIUS

--- src/constants/measurements.js
+++ src/constants/measurements.js
@@ -1,4 +1,6 @@
+import { store } from '../App';
+
 export const SCREEN_MARGIN = 20;
-export const BORDER_RADIUS = 15;
+export const BORDER_RADIUS = () => store && store.getState().app.get('squareBorders') ? 0 : 15;
 export const TOP_CARD_LIST_PADDING = 15;
 export const MAX_SUMMARY_LENGTH = 120;
