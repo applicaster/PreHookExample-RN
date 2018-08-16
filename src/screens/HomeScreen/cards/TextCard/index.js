f0cc6097940a3901d18bba26352dacb9ef00d500
Manuel Pretelin <m_pretelin@hotmail.com>
2018-08-16 10:13:41 -0500

    feat(TextCard): Add borderRadius prop

--- src/screens/HomeScreen/cards/TextCard/index.js
+++ src/screens/HomeScreen/cards/TextCard/index.js
@@ -6,7 +6,7 @@ import CardContainer from '../components/CardContainer';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { styles } from '../style';
-import { MAX_SUMMARY_LENGTH } from '../../../../constants/measurements';
+import { MAX_SUMMARY_LENGTH, BORDER_RADIUS } from '../../../../constants/measurements';
 
 export default class TextCard extends Component {
   activateCard() {
@@ -16,11 +16,12 @@ export default class TextCard extends Component {
 
   render() {
     const { caption, eventId } = this.props;
-    const backgroundColorStyle = { backgroundColor: this.context.styles.backgroundColor };
+    const { backgroundColor, borderType } = this.context.styles;
+    const backgroundColorStyle = { backgroundColor };
 
     return (
       <CardContainer>
-        <View style={[styles.eventContainer, backgroundColorStyle]}>
+        <View style={[styles.eventContainer, backgroundColorStyle, { borderRadius: BORDER_RADIUS(borderType) }]}>
           <Header eventId={eventId} overlay={false} />
           <Caption caption={caption} maxChar={MAX_SUMMARY_LENGTH * 4} />
           <Footer eventId={eventId} />
