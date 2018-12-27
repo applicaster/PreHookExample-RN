f381aa5046019e1e38aeae6e58ce811e89417e40
kononenkoAnton <a.kononenko@applicaster.com>
2018-12-27 18:14:59 -0500

    Remove from screen unused method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
-import { DeviceEventEmitter, NativeEventEmitter, NativeModules, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  DeviceEventEmitter,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 const { HookManager, ScreenPlugin } = NativeModules;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: 'blue',
-    borderColor: 'white',
+    backgroundColor: "blue",
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 12,
-    color: 'white',
+    color: "white",
     fontSize: 24,
-    fontWeight: 'bold',
-    overflow: 'hidden',
+    fontWeight: "bold",
+    overflow: "hidden",
     padding: 12,
-    textAlign: 'center',
+    textAlign: "center"
   },
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
 
 class App extends Component {
@@ -28,23 +37,29 @@ class App extends Component {
     this.screenWillDismissCallback = this.screenWillDismissCallback.bind(this);
 
     this.state = {
-      screenDimissed: false,
+      screenDimissed: false
     };
   }
 
   componentWillMount() {
-    const SCREEN_WILL_DISMISS = 'screen_will_dismiss';
-    
-    if (Platform.OS === 'ios') {
+    const SCREEN_WILL_DISMISS = "screen_will_dismiss";
+
+    if (Platform.OS === "ios") {
       const eventEmitter = new NativeEventEmitter(HookManager);
-      this.screenWillDismissSubscription = eventEmitter.addListener(SCREEN_WILL_DISMISS, this.screenWillDismissCallback);
+      this.screenWillDismissSubscription = eventEmitter.addListener(
+        SCREEN_WILL_DISMISS,
+        this.screenWillDismissCallback
+      );
     } else {
-      DeviceEventEmitter.addListener(SCREEN_WILL_DISMISS, this.screenWillDismissCallback);
+      DeviceEventEmitter.addListener(
+        SCREEN_WILL_DISMISS,
+        this.screenWillDismissCallback
+      );
     }
   }
 
   componentWillUnmount() {
-    if (Platform.OS === 'ios') {
+    if (Platform.OS === "ios") {
       this.screenWillDismissSubscription.remove();
       NativeEventEmitter.removeAllListeners();
     } else {
@@ -53,49 +68,48 @@ class App extends Component {
   }
 
   screenWillDismissCallback() {
-    HookManager.hookFinishedWork(false, null, { foo: 'bar' }, true);
+    HookManager.hookFinishedWork(false, null, { foo: "bar" }, true);
   }
 
   hookFinishedWithSuccess() {
-    HookManager.hookFinishedWork(true, null, { foo: 'bar' }, false);
+    HookManager.hookFinishedWork(true, null, { foo: "bar" }, false);
   }
 
   hookFinishedWithSuccessWithRemoveFromStack() {
-    HookManager.hookFinishedWork(true, null, { foo: 'bar' }, false);
+    HookManager.hookFinishedWork(true, null, { foo: "bar" }, false);
     ScreenPlugin.removeScreenPluginFromNavigationStack();
   }
-  
+
   hookFinishedWithFail() {
-    HookManager.hookFinishedWork(false, null, { foo: 'bar' }, false);
+    HookManager.hookFinishedWork(false, null, { foo: "bar" }, false);
   }
 
   hookFinishedWithFailBlockFlow() {
-    HookManager.hookFinishedWork(false, null, { foo: 'bar' }, true);
-    ScreenPlugin.removeScreenPluginFromScreen();
+    HookManager.hookFinishedWork(false, null, { foo: "bar" }, true);
   }
 
   render() {
     return (
-      <View style={ styles.container }>
+      <View style={styles.container}>
         <View>
           <TouchableOpacity onPress={this.hookFinishedWithSuccess}>
             <Text style={styles.button}>Hook Finished Success</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={this.hookFinishedWithSuccessWithRemoveFromStack}>
-            <Text style={styles.button}>Hook Finished Success Remove From Stack</Text>
+          <TouchableOpacity
+            onPress={this.hookFinishedWithSuccessWithRemoveFromStack}
+          >
+            <Text style={styles.button}>
+              Hook Finished Success Remove From Stack
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity onPress={this.hookFinishedWithFail}>
             <Text style={styles.button}>Hook Finished Fail</Text>
           </TouchableOpacity>
 
-            <TouchableOpacity onPress={this.hookFinishedWithFailBlockFlow}>
+          <TouchableOpacity onPress={this.hookFinishedWithFailBlockFlow}>
             <Text style={styles.button}>Hook Finished Fail and block flow</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => { ScreenPlugin.removeScreenPluginFromScreen(); }}>
-            <Text style={styles.button}>Remove Screen Plugin From Screen</Text>
           </TouchableOpacity>
         </View>
       </View>
